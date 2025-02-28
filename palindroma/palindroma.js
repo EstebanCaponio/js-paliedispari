03a95383c7a97b8745b4dda244a6812c935aef1b
EstebanCaponio <estebancaponio@gmail.com>
2025-02-28 17:02:29 +0100

    feat: created function with for to reverse word and condition

--- palindroma/palindroma.js
+++ palindroma/palindroma.js
@@ -1,3 +1,21 @@
 // Chiedere all’utente di inserire una parola;
 let word = prompt('Inserisci una parola per verificare che sia Palindroma');
 
+// creo funzione palindroma
+function palindroma(word) {
+
+    // soluzione facile
+    // formattazione parola/frase + creo array, lo inverto e lo riconverto a stringa
+    // word = word.replace(/\s+/g, '').toLowerCase();
+    // let reverseWord = word.split("").reverse().join("");
+
+    // soluzione con ciclo
+    let reverseWord = '';
+    for (i = (word.length - 1); i >= 0; i--) {
+        reverseWord += word[i];
+    }
+
+    if (word === reverseWord) {
+        return true;
+    } return false;
+}
